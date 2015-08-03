487a6d10c0cfb9a197503756fd6b2746ed3a44a6
Jaime Buelta <jaime.buelta@gmail.com>
2015-08-03 16:32:35 +0100

    Improve the examples.py file

--- examples.py
+++ examples.py
@@ -3,12 +3,16 @@ from gorgon import Gorgon
 import requests
 import hashlib
 
+
 def operation_http(number):
     result = requests.get('http://localhost')
+    # The result could be used to perform more operations, making complex
+    # workflows
     return result.status_code
 
 
 def operation_hash(number):
+    # This is just an example of a computationally expensive task
     m = hashlib.sha512()
     for _ in range(400):
         m.update('TEXT {}'.format(number).encode())
@@ -21,18 +25,23 @@ def operation_hash(number):
 
 def test():
     NUM_OPS=40000
-    # for i in range(NUM_OPS):
-    #     operation(i)
 
+    # Multiple goes can be used for ramping up
     test = Gorgon(operation_hash)
     test.go(num_operations=NUM_OPS, num_processes=1, num_threads=1)
-    # test.print_report()
     test.go(num_operations=NUM_OPS, num_processes=10, num_threads=10)
-    # test.print_report()
     test.go(num_operations=NUM_OPS, num_processes=20, num_threads=10)
-    # test.print_report()
     test.go(num_operations=NUM_OPS, num_processes=40, num_threads=50)
-    # print(test.small_report())
+    print(test.small_report())
+    print(test.html_graph_report())
+
+    # HTTP testing is easy
+    test = Gorgon(operation_http)
+    test.go(num_operations=NUM_OPS, num_processes=1, num_threads=1)
+    test.go(num_operations=NUM_OPS, num_processes=10, num_threads=10)
+    test.go(num_operations=NUM_OPS, num_processes=20, num_threads=10)
+    test.go(num_operations=NUM_OPS, num_processes=40, num_threads=50)
+    print(test.small_report())
     print(test.html_graph_report())
 
 if __name__ == '__main__':
