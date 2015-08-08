c6c9138b54be0194731ef18f1f1608350b6e6343
Jaime Buelta <jaime.buelta@gmail.com>
2015-08-08 17:06:51 +0100

    Release 0.3.2
    Fixing distribution problems

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ setup(
     name='gorgon',
     description='A simple multiplier task analysis tool in Python',
     license='MIT',
-    version='0.3.1',
+    version='0.3.2',
     author='Jaime Buelta',
     author_email='jaime.buelta@gmail.com',
     url='https://github.com/jaimebuelta/gorgon',
-    download_url='https://github.com/jaimebuelta/gorgon/tarball/0.3.1',
+    download_url='https://github.com/jaimebuelta/gorgon/tarball/0.3.2',
     platforms='Cross Platform',
     classifiers=[
         'Programming Language :: Python :: 2',
